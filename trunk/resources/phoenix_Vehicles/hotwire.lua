e5ebef2a32b3b97b63421cb0289026f765c3916b
jyrno42 <jyrno42@bb664bb8-a752-11de-a118-c7387b1e4f66>
2010-10-08 17:00:05 +0000

    Rakendasin patchi koige viimaste muudatustega v5...

--- trunk/resources/phoenix_Vehicles/hotwire.lua
+++ trunk/resources/phoenix_Vehicles/hotwire.lua
@@ -66,3 +66,71 @@ addEvent( "onCarWireStart", true );
 addEventHandler( "onCarWireStart", rootElement, showLoadBar );
 
 addEventHandler( "onClientElementDataChange", getVehicle, function ( var ) outputDebugString( tostring( var ) ) end );
+local thePlayer = nil;
+local theVehicle = nil;
+
+local rootElement = getRootElement( );
+local guiLoadBar = nil;
+local theTimer = nil;
+
+function InitHotwire( )
+
+	thePlayer = getLocalPlayer( );
+	
+	guiLoadBar = guiCreateProgressBar( 0.45, .7, .1, .1, true );
+	guiProgressBarSetProgress( guiLoadBar, 0.0 );
+	
+	guiSetVisible( guiLoadBar, false );
+
+end
+
+addEventHandler( "onClientResourceStart", getResourceRootElement( getThisResource( ) ), InitHotwire );
+
+function hideLoadBar( )
+
+	if( theTimer and isTimer( theTimer ) ) then
+	
+		killTimer( theTimer );
+	
+	end
+	
+	guiSetVisible( guiLoadBar, false );
+
+end
+
+function hotWireFunc( )
+
+	if( not isPedInVehicle( thePlayer ) or getPedOccupiedVehicle( thePlayer ) ~= theVehicle ) then
+	
+		hideLoadBar( );
+		return false;
+	
+	end
+	
+	local progress = guiProgressBarGetProgress( guiLoadBar ) + 5;
+		
+	guiProgressBarSetProgress( guiLoadBar, progress );
+	if( progress >= 100 ) then
+	
+		hideLoadBar( );
+		triggerServerEvent( "onWireStarted", thePlayer );
+	
+	end
+
+end
+
+function showLoadBar( theVeh )
+
+	theVehicle = theVeh;
+	hideLoadBar( );
+
+	guiSetVisible( guiLoadBar, true );
+	guiProgressBarSetProgress( guiLoadBar, 0.0 );
+	
+	theTimer = setTimer( hotWireFunc, 300, 0 );
+
+end
+addEvent( "onCarWireStart", true );
+addEventHandler( "onCarWireStart", rootElement, showLoadBar );
+
+addEventHandler( "onClientElementDataChange", getVehicle, function ( var ) outputDebugString( tostring( var ) ) end );
--- trunk/resources/phoenix_Vehicles/vehSecurity.lua
+++ trunk/resources/phoenix_Vehicles/vehSecurity.lua
@@ -132,3 +132,137 @@ function getLockLevel( theVehicle )
 	return 1;
 
 end
+local vehSecurity = {
+
+	["hotwire"] = "hw",
+	
+	["lock1"] = "l1",
+	["lock2"] = "l2",
+	["lock3"] = "l3",
+	["lock4"] = "l4",
+	["lock5"] = "l5",
+	["lock6"] = "l6",
+	["lock7"] = "l7"
+	
+};
+
+local sVehSecurity = { };
+for k, v in pairs( vehSecurity ) do
+
+  sVehSecurity[v] = k;
+ 
+end
+
+local securityData = { };
+
+function tokenize( theStr, delim, separator )
+	
+	local tbl = { };
+	local i = delim - 1;
+	while( true ) do
+	
+		i = i + 1;
+		local str = gettok( theStr, i, string.byte(separator) );
+		if( not str ) then break; end
+		tbl[str] = 1;
+	
+	end
+
+	return tbl;
+
+end
+
+function InitSecurity( theVehicle, theData )
+	
+	if( theVehicle and isElement( theVehicle ) and getElementType( theVehicle ) == "vehicle" ) then
+	
+		securityData[theVehicle] = tokenize( theData, 1, "," );
+		
+		for k, v in pairs( securityData[theVehicle] ) do
+		
+			if( not sVehSecurity[k] ) then 
+			
+				securityData[theVehicle][k] = nil;
+			
+			end
+		
+		end
+		
+		setElementData( theVehicle, "vLockLevel", true, getLockLevel( theVehicle ) );
+		
+	end
+
+end
+
+function SaveSecurity( theVehicle )
+
+	local str = "";
+	if( theVehicle and isElement( theVehicle ) and getElementType( theVehicle ) == "vehicle" ) then
+	
+		for k, v in pairs( securityData[theVehicle] ) do
+		
+			if( str ~= "" ) then str = str .. ","; end
+			str = str .. k;
+		
+		end
+	
+	end
+	return str;
+
+end
+
+function getVehicleSecurity( theVehicle, theSecurity )
+
+	if( theVehicle and isElement( theVehicle ) and getElementType( theVehicle ) == "vehicle" and securityData[theVehicle] ) then
+	
+		local theKey = vehSecurity[theSecurity] or theSecurity;
+	
+		if( theKey ) then
+		
+			return ( securityData[theVehicle][theKey] == 1 );
+		
+		end
+	
+	end
+	return false;
+
+end
+
+function setVehicleSecurity( theVehicle, theSecurity, unset )
+
+	if( theVehicle and isElement( theVehicle ) and getElementType( theVehicle ) == "vehicle" and securityData[theVehicle] ) then
+	
+		local theKey = vehSecurity[theSecurity] or theSecurity;
+	
+		if( theKey ) then
+		
+			if( not unset ) then
+			
+				securityData[theVehicle][theKey] = 1;
+			
+			else
+			
+				securityData[theVehicle][theKey] = nil;
+			
+			end
+			
+			setElementData( theVehicle, "vLockLevel", true, getLockLevel( theVehicle ) );
+		
+		end
+	
+	end
+
+end
+
+
+function getLockLevel( theVehicle )
+
+	for k = 7, 1, -1 do
+	
+		if( getVehicleSecurity( theVehicle, "lock" .. k ) ) then return k; end
+	
+	end
+	
+	return 1;
+
+end
